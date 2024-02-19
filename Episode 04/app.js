f1c49fce84bea1cbd984b9e3b764f7bffd0b09f6
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-02-19 19:57:53 +0530

    app building planning done episode 4

---
+++ Episode 04/app.js
@@ -0,0 +1,19 @@
+// importing react from node_module as React
+import React from 'react'
+import ReactDOM  from 'react-dom'
+
+/* Food Ordering App
+    Header
+        -Logo
+        -Nav items
+    Body
+        - Search
+        - ResturantContainer
+            -ResturantCard
+    Footer
+        - Copyright
+        - Address 
+        - Contact 
+        - links
+
+*/
