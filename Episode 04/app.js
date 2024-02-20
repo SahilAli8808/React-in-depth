95290610011e6ed1127e7b6ee0eed8d1052ba5e2
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-02-20 15:44:39 +0530

    food ordering app intialize

--- Episode 04/app.js
+++ Episode 04/app.js
@@ -1,19 +1,42 @@
 // importing react from node_module as React
 import React from 'react'
-import ReactDOM  from 'react-dom'
+import ReactDOM  from 'react-dom/client'
 
-/* Food Ordering App
-    Header
-        -Logo
-        -Nav items
-    Body
-        - Search
-        - ResturantContainer
-            -ResturantCard
-    Footer
-        - Copyright
-        - Address 
-        - Contact 
-        - links
+const Header =()=>{
+    return (
+        <div className='header'>
+            this is Header
+        </div>
+    )
+}
 
-*/
+const Body = ()=>{
+    return (
+        <div className='body'>
+            This is Body
+        </div>
+    )
+}
+
+const Footer = ()=>{
+    return (
+        <div className='footer'>
+            this is footer
+        </div>
+    )
+}
+
+const App=  ()=>{
+    return   (
+        <div>
+
+        <Header/>
+        <Body/>
+        <Footer/>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
+
